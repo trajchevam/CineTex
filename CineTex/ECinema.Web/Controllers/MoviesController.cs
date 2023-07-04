a2e4d14d90cf0f0736c5b35f9635ed62bbad230b
trajchevaM <118018439+trajchevaM@users.noreply.github.com>
2023-07-05 00:35:39 +0200

    Initial commit

---
+++ CineTex/ECinema.Web/Controllers/MoviesController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ECinema.Web.Data;
+using ECinema.Web.Models.DomainModels;
+
+namespace ECinema.Web.Controllers
+{
+    public class MoviesController : Controller 
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movies.ToListAsync());
+        }
+
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return View(movie);
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MovieName,MovieImage,Language,Genre")] Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                movie.Id = Guid.NewGuid();
+                _context.Add(movie);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var m = await _context.Movies.FindAsync(id);
+            if (m == null)
+            {
+                return NotFound();
+            }
+            return View(m);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Id,MovieName,MovieImage,Language,Genre")] Movie movie)
+        {
+            var m = movie;
+            if (id != m.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(m);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MovieExists(m.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(m);
+        }
+
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var m = await _context.Movies
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (m == null)
+            {
+                return NotFound();
+            }
+
+            return View(m);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var m = await _context.Movies.FindAsync(id);
+            _context.Movies.Remove(m);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MovieExists(Guid id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CineTex/ECinema.Web/Models/DomainModels/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECinema.Web.Models.DomainModels
+{
+    public class Actor
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+        public DateTime  DateOfBirth { get; set; }
+        public string AboutActor { get; set; }
+        public virtual ICollection<ActorsInMovies> ActorsInMovies { get; set; }
+    }
+}
